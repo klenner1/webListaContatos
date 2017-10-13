6d9b7ecc8b000a5561f233fac9a7bb8ac00c0ef1
klenner1 <32469807+klenner1@users.noreply.github.com>
2017-10-13 14:19:26 -0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,29 @@
+﻿var lista = [];
+var div = document.getElementById("lista");
+var result = "";
+function add() {
+        var nome = document.getElementById("Nome").value;
+        var numero = document.getElementById("Numero").value;
+        if (nome == NaN || nome == null || nome == "" || numero == NaN || nome == numero || numero == "") {
+            alert("erro");
+        }
+        else {
+            lista.push("Nome: "+nome + " - Telefone: " +numero);
+			result="";
+            for (var i = 0; i < lista.length; i++) {
+                result = result + "<li class='mdl-list__item'>" +
+				"<span class='mdl-list__item-primary-content'>"+
+				"<i class='material-icons mdl-list__item-icon'>person</i>" +
+				lista[i] + "</span></li>";
+            }
+            imprime();
+        }   
+}
+function remover() {
+	div.innerHTML = "";
+    var result = "";
+	lista = [];
+}
+function imprime() {
+    div.innerHTML = result;
+}
